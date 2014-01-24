6902685ee07b8d402b2117cef1f97c23d5fc5dd5
Rafael Corral <rxcorral@gmail.com>
2014-01-23 22:16:41 -0500

    Improved algorithm, also added a message that displays the end game status, either a draw message or who won.

--- js/main.js
+++ js/main.js
@@ -4,23 +4,33 @@ requirejs.config({
 
 require(['templates', 'ttt'], function(templates, game) {
     var $board = jQuery('.board-wrapper'),
-        callback = function(indexes) {
-            _.each(indexes, function(i) {
-                jQuery('[data-index='+i+']').addClass('winning');
-            });
+        callback = function(indexes, message) {
+            // If there is a length on indexes highlight those indexes
+            if (indexes.length) {
+                _.each(indexes, function(i) {
+                    jQuery('[data-index='+i+']').addClass('winning');
+                });
+            }
+
+            if (message === 'draw') {message = "It's a draw!"};
+            if (message === 'ai') {message = "The computer wins!"};
+            if (message === 'user') {message = "You won, congrats!"};
+
+            $board.find('.board-message').html(message);
         };
 
     // Auto init game as the user playing first
     game.init({
         board: $board,
-        user_piece: 'o',
+        user_piece: 'x',
         callback: callback
     });
-    $board.addClass('user-piece-o');
+    $board.addClass('user-piece-x');
 
     jQuery('[data-action~=restart]').on('click', function() {
         var users_piece = jQuery(this).data('begin-with') === 'ai' ? 'o' : 'x';
 
+        $board.find('.board-message').html('');
         game.init({
             board: $board,
             user_piece: users_piece,
